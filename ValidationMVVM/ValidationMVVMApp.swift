e217eed5958ba2d6503f5015efb368b9287c0834
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-07 13:53:11 +0700

    Initial Commit

---
+++ ValidationMVVM/ValidationMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidationMVVMApp.swift
+//  ValidationMVVM
+//
+//  Created by Ricky Primayuda Putra on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ValidationMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValidationMVVMTests/ValidationMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValidationMVVMTests.swift
+//  ValidationMVVMTests
+//
+//  Created by Ricky Primayuda Putra on 07/10/24.
+//
+
+import Testing
+@testable import ValidationMVVM
+
+struct ValidationMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
